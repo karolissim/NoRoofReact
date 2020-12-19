b14f57d700270b4cb8e54b8652d81ca2b092bf71
Karolis Simanavičius <56815036+knuckl35@users.noreply.github.com>
2020-12-19 21:44:32 +0200

    add nav bar and home screen

---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
+import './Navbar.css'
+
+class Navbar extends Component {
+    render() {
+        return (
+            <React.Fragment>
+                <div class="nav">
+                    <div class="logo">
+                        <Link to='/'>NoRoof.</Link>
+                    </div>
+                    <div class="nav-links">
+                        <Link to="/">Shop</Link>
+                        <Link to="/faq">FAQ</Link>
+                        <Link to="contact">Contact</Link>
+                        <a id="cart-trigger" href="#0">Cart</a>
+                    </div>
+                </div>
+                <div id="shadow-layer"></div>
+            </React.Fragment>
+        )
+    }
+}
+
+export default Navbar
